b05cbca3cdd0bca2520ffd8285071f5ac54a002a
omkaringawale21@gmail.com <omkaringawale21@gmail.com>
2023-12-13 18:58:56 +0530

    mern website update again

--- index.js
+++ index.js
@@ -30,84 +30,84 @@ Connection(userName, userPass, userDB);
 
 app.use(express.json());
 app.use(cookieParser("*"));
-app.use(cors({
-    origin: ["https://coursebuilder-n3k9.onrender.com/"],
+app.use(cors("*", {
+    origin: true,
     Credential: true,
 }));
 
-app.use(session({
-    secret: "omkaringawaleomkaringawaleomkaringawaleomkaringawale3456",
-    resave: false,
-    saveUninitialized: true,
-}));
+// app.use(session({
+//     secret: "omkaringawaleomkaringawaleomkaringawaleomkaringawale3456",
+//     resave: false,
+//     saveUninitialized: true,
+// }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.use(
-    new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_SECREAT,
-        callbackURL: "/google/callback",
-        scope: ["profile", "email"],
-    },
-        async (accessToken, refreshToken, profile, done) => {
-            try {
-                let userExits = await userCollections.findOne({ email: profile.emails[0].value });
+// passport.use(
+//     new GoogleStrategy({
+//         clientID: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_SECREAT,
+//         callbackURL: "/google/callback",
+//         scope: ["profile", "email"],
+//     },
+//         async (accessToken, refreshToken, profile, done) => {
+//             try {
+//                 let userExits = await userCollections.findOne({ email: profile.emails[0].value });
 
-                if (!userExits) {
-                    let newuser = new userCollections({
-                        fname: profile.displayName,
-                        email: profile.emails[0].value,
-                        password: profile.emails[0].value,
-                        image: profile.photos[0].value
-                    });
+//                 if (!userExits) {
+//                     let newuser = new userCollections({
+//                         fname: profile.displayName,
+//                         email: profile.emails[0].value,
+//                         password: profile.emails[0].value,
+//                         image: profile.photos[0].value
+//                     });
 
-                    await newuser.save();
+//                     await newuser.save();
 
-                    const token = jwt.sign({ _id: newuser._id }, process.env.SECREAT_KEY_JWT);
+//                     const token = jwt.sign({ _id: newuser._id }, process.env.SECREAT_KEY_JWT);
 
-                    GoogleMiddleware(token);
-                } else {
-                    const token = jwt.sign({ _id: userExits._id }, process.env.SECREAT_KEY_JWT);
+//                     GoogleMiddleware(token);
+//                 } else {
+//                     const token = jwt.sign({ _id: userExits._id }, process.env.SECREAT_KEY_JWT);
 
-                    GoogleMiddleware(token);
-                }
+//                     GoogleMiddleware(token);
+//                 }
 
-                return done(null, profile);
-            } catch (error) {
-                return done(error, null);
-            }
+//                 return done(null, profile);
+//             } catch (error) {
+//                 return done(error, null);
+//             }
 
-        }
-    )
-);
+//         }
+//     )
+// );
 
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
+// passport.serializeUser((user, done) => {
+//     done(null, user);
+// });
 
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
+// passport.deserializeUser((user, done) => {
+//     done(null, user);
+// });
 
-app.get("/google", passport.authenticate("google", { scope: ["profile", "'email"] }));
+// app.get("/google", passport.authenticate("google", { scope: ["profile", "'email"] }));
 
-app.get("/google/callback", passport.authenticate("google", {
-    successRedirect: "/course",
-    failureRedirect: "/login",
-}));
+// app.get("/google/callback", passport.authenticate("google", {
+//     successRedirect: "/course",
+//     failureRedirect: "/login",
+// }));
 
-app.get("/google/logout", (request, response, next) => {
-    request.logOut(
-        function (err) {
-            if (err) {
-                return next(err);
-            }
-            response.redirect("/login");
-        }
-    )
-});
+// app.get("/google/logout", (request, response, next) => {
+//     request.logOut(
+//         function (err) {
+//             if (err) {
+//                 return next(err);
+//             }
+//             response.redirect("/login");
+//         }
+//     )
+// });
 
 app.use(router);
 
